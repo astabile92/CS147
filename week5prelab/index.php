4a3ad5734655742af8baa970b455dedaf853443c
Alex Stabile <astabile@stanford.edu>
2012-10-23 01:32:04 -0700

    added jquery mobile to login prelab

--- week5prelab/index.php
+++ week5prelab/index.php
@@ -10,11 +10,23 @@
 		<meta name="apple-mobile-web-app-capable" content="yes">
 		<meta name="apple-mobile-web-app-status-bar-style" content="black">
 		<meta name="viewport" content="width=device-width, user-scalable=no" />
-		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+		
+		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
+		<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+		<script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
+		
 		<link href="style.css" rel="stylesheet" type="text/css">
 	</head>
 
 	<body>
+	
+	<div data-role="page">
+
+	<div data-role="header">
+		<h1>Login Page</h1>
+	</div><!-- /header -->
+
+	<div data-role="content">	
 	
 		<?php
 			if (isset($_SESSION['error'])) {
@@ -24,16 +36,15 @@
 		?>
 	
 		<form action="submit.php" method="post" id="login">			
-			<h2><span>Login:</span></h2>
-			<label>Username
-			<input class="forminput" type="text" id="username" name="username" autocapitalize="off" />
-			</label>
-								
-			<label>Password
-			<input class="forminput" type="password" id="password" name="password" />
-			</label>
+			<label for="username" class="ui-hidden-accessible">Username</label>
+			<input type="text" autocapitalize="off" name="username" id="username" value="" placeholder="Username"/>
+			
+			<label for="password" class="ui-hidden-accessible">Password</label>
+			<input type="password" name="password" id="password" value="" placeholder="Password"/>
 														
 			<input type="submit" value="Login" />					
 			</form>	
+	</div>
+	</div><!-- /content -->
 	</body>
 </html>
